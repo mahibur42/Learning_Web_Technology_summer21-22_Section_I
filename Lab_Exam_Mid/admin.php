635f0900934959d5173c3b8dd88a3969f8a77f5e
mahibur42 <106877147+mahibur42@users.noreply.github.com>
2022-06-20 13:20:42 +0600

    Add files via upload

---
+++ Lab_Exam_Mid/admin.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+  
+        <fieldset>
+            <h1>Welcome Bob!</h1>
+            <br><a href = "profile.php"> Profile </a> 
+            <br><a href = "cngpass.php"> Change password </a> 
+            <br><a href = "view.php">    View user </a> 
+            <br><a href = "Logout.php">  Log out </a> 
+
+</fieldset>
+</body>
+</html>
